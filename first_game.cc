b8fa8842fde2646e2cbba990d1cf4aaf7ffd9a56
Gabe Wachob <gabe@tapcentive.com>
2020-12-13 19:01:00 -0800

    Dipping our toes in the eigen water.

--- first_game.cc
+++ first_game.cc
@@ -1,4 +1,5 @@
-
+#include <Eigen/Core>
+#include <Eigen/Geometry>
 #include <algorithm>
 #include <chrono>
 #include <functional>
@@ -6,6 +7,7 @@
 #include <ncurses.h>
 #include <thread>
 #include <vector>
+
 #define PI 3.14159265
 
 bool exitRequested = false;
@@ -20,10 +22,8 @@ std::thread timer_start(std::function<void(void)> func, unsigned int interval) {
     }
   });
 }
-struct Point2D {
-  int x = 0;
-  int y = 0;
-};
+
+using Point2D = Eigen::Vector2i;
 
 struct Item {
   char label;
@@ -34,47 +34,48 @@ struct Item {
 
 Point2D rotatePoint(Point2D start, Point2D center, int degrees) {
 
+
   float s = sin(degrees * PI / 180.0);
   float c = cos(degrees * PI / 180.0);
 
-  float resultX = start.x - center.x;
-  float resultY = start.y - center.y;
+  float resultX = start(0) - center(0);
+  float resultY = start(1) - center(1);
 
   // Rotation matrix is calculated here
   float dx = resultX * c - resultY * s;
   float dy = resultX * s + resultY * c;
 
-  return Point2D{int(center.x + dx), int(center.y + dy)};
+  return Point2D{int(center(0) + dx), int(center(1) + dy)};
 }
 
 void drawLine(Point2D start, Point2D end, WINDOW *window, char symbol) {
   // Using Bresenham's line algorithm from
   // https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
   Point2D current = start;
-  int dx = abs(end.x - start.x);
-  int sx = start.x < end.x ? 1 : -1;
+  int dx = abs(end(0) - start(0));
+  int sx = start(0) < end(0) ? 1 : -1;
 
-  int dy = -abs(end.y - start.y);
-  int sy = start.y < end.y ? 1 : -1;
+  int dy = -abs(end(1) - start(1));
+  int sy = start(1) < end(1) ? 1 : -1;
 
   int err = dx + dy;
 
   while (true) {
-    mvwaddch(window, current.y, current.x, symbol);
+    mvwaddch(window, current(1), current(0), symbol);
 
-    if ((current.x == end.x) && (current.y == end.y))
+    if ((current(0) == end(0)) && (current(1) == end(1)))
       break;
 
     int e2 = 2 * err;
 
     if (e2 >= dy) {
       err += dy;
-      current.x += sx;
+      current(0) += sx;
     }
 
     if (e2 < dx) {
       err += dx;
-      current.y += sy;
+      current(1) += sy;
     }
   }
 }
@@ -101,7 +102,8 @@ template <typename T, class... Args> struct Aged {
 
 int loop = 0;
 // We'll put game state here
-Point2D cursor{10, 10};
+Eigen::Vector2i userCursor;
+;
 int timeSpeed = 1;
 
 template <class T> class AgedVector {
@@ -154,36 +156,34 @@ void addSquare(WINDOW *window, Point2D center) {
   squares.add(RotatingSquare('.', center, 2, 10.0));
 }
 
-void drawSquare(WINDOW *window, const RotatingSquare& square, int angle) {
-  int leftX = square.center.x - square.size;
-    int rightX = square.center.x + square.size;
-    int topY = square.center.y - square.size;
-    int bottomY = square.center.y + square.size;
+void drawSquare(WINDOW *window, const RotatingSquare &square, int angle) {
+  int leftX = square.center(0) - square.size;
+  int rightX = square.center(0) + square.size;
+  int topY = square.center(1) - square.size;
+  int bottomY = square.center(1) + square.size;
 
-    // I don't like that the calculation of the rotation angle here has to use
-    // timeSpeed again, when we already passed it in to the squares via tick()
+  // I don't like that the calculation of the rotation angle here has to use
+  // timeSpeed again, when we already passed it in to the squares via tick()
 
-    // Maybe we shouldn't bother with a AgedVector for boxes? Or augment it?
+  // Maybe we shouldn't bother with a AgedVector for boxes? Or augment it?
 
-    // Calculate rotated/translated
-    Point2D upperLeft = rotatePoint(
-        Point2D{leftX, topY}, square.center, angle);
-    Point2D upperRight = rotatePoint(
-        Point2D{rightX, topY}, square.center, angle);
-    Point2D lowerLeft = rotatePoint(
-        Point2D{leftX, bottomY}, square.center, angle);
-    Point2D lowerRight = rotatePoint(
-        Point2D{rightX, bottomY}, square.center, angle);
+  // Calculate rotated/translated
+  Point2D upperLeft = rotatePoint(Point2D{leftX, topY}, square.center, angle);
+  Point2D upperRight = rotatePoint(Point2D{rightX, topY}, square.center, angle);
+  Point2D lowerLeft =
+      rotatePoint(Point2D{leftX, bottomY}, square.center, angle);
+  Point2D lowerRight =
+      rotatePoint(Point2D{rightX, bottomY}, square.center, angle);
 
-    // Now draw 4 lines
-    drawLine(upperLeft, upperRight, window, square.label);
-    drawLine(upperLeft, lowerLeft, window, square.label);
-    drawLine(upperRight, lowerRight, window, square.label);
-    drawLine(lowerLeft, lowerRight, window, square.label);
-    mvwaddch(window, upperLeft.y, upperLeft.x, '+');
-    mvwaddch(window, upperRight.y, upperRight.x, '+');
-    mvwaddch(window, lowerLeft.y, lowerLeft.x, '+');
-    mvwaddch(window, lowerRight.y, lowerRight.x, '+');
+  // Now draw 4 lines
+  drawLine(upperLeft, upperRight, window, square.label);
+  drawLine(upperLeft, lowerLeft, window, square.label);
+  drawLine(upperRight, lowerRight, window, square.label);
+  drawLine(lowerLeft, lowerRight, window, square.label);
+  mvwaddch(window, upperLeft(1), upperLeft(0), '+');
+  mvwaddch(window, upperRight(1), upperRight(0), '+');
+  mvwaddch(window, lowerLeft(1), lowerLeft(0), '+');
+  mvwaddch(window, lowerRight(1), lowerRight(0), '+');
 }
 
 void GameLoop(WINDOW *window) {
@@ -202,16 +202,16 @@ void GameLoop(WINDOW *window) {
     return;
     break;
   case KEY_LEFT:
-    cursor.x = std::max(cursor.x - 1, minX + 1);
+    userCursor(0) = std::max(userCursor(0) - 1, minX + 1);
     break;
   case KEY_RIGHT:
-    cursor.x = std::min(cursor.x + 1, maxX - 2);
+    userCursor(0) = std::min(userCursor(0) + 1, maxX - 2);
     break;
   case KEY_UP:
-    cursor.y = std::max(cursor.y - 1, minY + 1);
+    userCursor(1) = std::max(userCursor(1) - 1, minY + 1);
     break;
   case KEY_DOWN:
-    cursor.y = std::min(cursor.y + 1, maxY - 2);
+    userCursor(1) = std::min(userCursor(1) + 1, maxY - 2);
     break;
   case '+':
     timeSpeed++;
@@ -226,10 +226,10 @@ void GameLoop(WINDOW *window) {
     }
     break;
   case 's':
-    addSquare(window, cursor);
+    addSquare(window, userCursor);
     break;
   case ' ':
-    addDecaying(window, cursor);
+    addDecaying(window, userCursor);
     break;
   }
 
@@ -242,18 +242,19 @@ void GameLoop(WINDOW *window) {
   timeoutList.tick(timeSpeed);
   squares.tick(timeSpeed);
   for (const auto &item : timeoutList) {
-    mvwaddch(window, item.item.center.y, item.item.center.x,
+    mvwaddch(window, item.item.center(1), item.item.center(0),
              item.item.label |
                  COLOR_PAIR((timeoutList.getAge() - item.birthTime) / 10 + 50));
   }
 
   for (const auto &agedSquare : squares) {
     const RotatingSquare &square = agedSquare.item;
-    int angle = (square.rotationSpeed * timeSpeed * (loop - agedSquare.birthTime));
+    int angle =
+        (square.rotationSpeed * timeSpeed * (loop - agedSquare.birthTime));
     drawSquare(window, square, angle);
   }
 
-  mvwaddch(window, cursor.y, cursor.x, 'X' | A_BOLD);
+  mvwaddch(window, userCursor(1), userCursor(0), 'X' | A_BOLD);
   wrefresh(window); /* Print it on to the real screen */
 }
 
@@ -266,6 +267,7 @@ int main() {
   nodelay(stdscr, TRUE);
   keypad(stdscr, TRUE);
   wresize(window, 300, 300);
+  userCursor << 10, 10;
   start_color();
   for (int i = 50; i < 60; i++) {
     init_color(i, (200 - (i - 50) * 20), (1000 - (i - 50) * 100),
